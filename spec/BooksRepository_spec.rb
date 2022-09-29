cb810d0ba2a3ba7248083a11d2d09be1e8635ef6
Sarita <saritaradia65@gmail.com>
2022-09-29 15:53:26 +0100

    Refactor

--- spec/BooksRepository_spec.rb
+++ spec/BooksRepository_spec.rb
@@ -1,12 +1,12 @@
 require '/Users/saritaradia/Desktop/Projects/book_store/lib/BooksRepository.rb'
 
+RSpec.describe BooksRepository do 
 def reset_books_table
   seed_sql = File.read('spec/seeds_books.sql')
   connection = PG.connect({ host: '127.0.0.1', dbname: 'book_store_test' })
   connection.exec(seed_sql)
 end
 
-describe BooksRepository do
   before(:each) do
     reset_books_table
   end
@@ -20,3 +20,4 @@ describe BooksRepository do
     expect(books[0].author_name).to eq('Matt Haig')
   end
 end
+ 
